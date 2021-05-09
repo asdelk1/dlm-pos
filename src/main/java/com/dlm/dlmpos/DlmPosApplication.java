8e36e21b171c79dbbadf8d3e7873e7e149eef9bb
Asdelk <w.dealwis@ucdconnect.ie>
2021-05-09 11:58:36 +0530

    Added client

--- src/main/java/com/dlm/dlmpos/DlmPosApplication.java
+++ src/main/java/com/dlm/dlmpos/DlmPosApplication.java
@@ -1,7 +1,9 @@
 package com.dlm.dlmpos;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class DlmPosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DlmPosApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
     }
 
 }
--- src/main/java/com/dlm/dlmpos/controller/ItemController.java
+++ src/main/java/com/dlm/dlmpos/controller/ItemController.java
@@ -1,9 +1,37 @@
 package com.dlm.dlmpos.controller;
 
+import com.dlm.dlmpos.dto.ItemDTO;
+import com.dlm.dlmpos.entity.Item;
+import com.dlm.dlmpos.service.ItemService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    private final ModelMapper mapper;
+
+    public ItemController(ItemService itemService, ModelMapper mapper) {
+        this.itemService = itemService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ItemDTO>> listItems(){
+
+        List<Item> items = this.itemService.getAll();
+        List<ItemDTO> dtoList = items.stream().map(i -> this.mapper.map(i, ItemDTO.class)).collect(Collectors.toList());
+        return ResponseEntity.ok(dtoList);
+    }
+
 }
