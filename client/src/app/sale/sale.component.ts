d3b62c11e2ae9c9744a2140fbc1d6c943249e0e5
Asdelk <w.dealwis@ucdconnect.ie>
2021-05-22 14:19:24 +0530

    Added new dialog receive money

--- client/src/app/sale/sale.component.ts
+++ client/src/app/sale/sale.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ItemService} from "../items/item.service";
 import {Item} from "../items/item.model";
 import {SaleDetail} from "./sale.model";
@@ -7,6 +7,7 @@ import {Observable} from "rxjs";
 import {map, startWith} from 'rxjs/operators';
 import {SaleService} from "./sale.service";
 import {NotificationsService, NotificationType} from "../notifications/notifications.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'app-sale',
@@ -20,15 +21,21 @@ export class SaleComponent implements OnInit {
     public itemDict: { [itemId: string]: Item } = {};
     public sales: SaleDetail[] = [];
     public total: number = 0.00;
+    public balance: number = 0;
+
+    @ViewChild("modalData")
+    public saleDialog: ElementRef;
 
     public form: FormGroup = new FormGroup({
         "item": new FormControl("", [Validators.required]),
         "qty": new FormControl("", [Validators.required])
     });
+    public receivedAmount: FormControl = new FormControl("", [Validators.required]);
 
     constructor(private itemService: ItemService,
                 private saleService: SaleService,
-                private notificationService: NotificationsService
+                private notificationService: NotificationsService,
+                private modalService: NgbModal
     ) {
 
     }
@@ -41,9 +48,6 @@ export class SaleComponent implements OnInit {
             }
         )
 
-        this.form.get("item").valueChanges.subscribe(
-            (value => console.log(value)));
-
         this.filteredItems = this.form.get("item").valueChanges
             .pipe(
                 startWith(''),
@@ -51,6 +55,12 @@ export class SaleComponent implements OnInit {
                     return item ? this._filterItems(item) : this.items.slice();
                 })
             );
+
+        this.receivedAmount.valueChanges.subscribe(
+            (value: number) => {
+                 this.balance = value - this.total;
+            }
+        );
     }
 
     private _filterItems(value: string): Item[] {
@@ -81,15 +91,24 @@ export class SaleComponent implements OnInit {
         this.form.reset({item: "", qty: ""});
     }
 
-    public onShoppingCart(): void {
+    public processCart(): void {
+        this.modalService.open(this.saleDialog, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+            this.printReceipt();
+        }, (res) => {
+            this.resetCart();
+        });
+    }
+
+    public receiveMoney(): void {
         this.saleService.saveShoppingCart(this.sales).subscribe(
             (items: string) => {
                 this.notificationService.showNotification("Success", NotificationType.SUCCESS);
+
                 this.resetCart();
             });
     }
 
-    public printRecipt(): void {
+    public printReceipt(): void {
         this.saleService.printReceipt(11).subscribe(
             (response: any) => {
                 let blob = new Blob([response], {type: "application/pdf"});
