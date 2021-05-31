e264258579885e378f2e99b7de9dbff6abbcf987
Asdelk <w.dealwis@ucdconnect.ie>
2021-05-31 22:17:07 +0530

    Deleted unnecessary components

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -11,17 +11,10 @@ declare interface RouteInfo {
     adminOnly: boolean;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '', adminOnly: true },
     { path: '/sale', title: 'Sale',  icon: 'shopping_cart', class: '', adminOnly: false  },
     { path: '/items', title: 'Items',  icon: 'dns', class: '', adminOnly: true  },
     { path: '/history', title: 'Previous Sales',  icon: 'restore', class: '', adminOnly: true  },
     { path: '/user-profile', title: 'User Profile',  icon:'person', class: '', adminOnly: true  },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '', adminOnly: true  },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '', adminOnly: true  },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '', adminOnly: true  },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '', adminOnly: true  },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '', adminOnly: true  },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro', adminOnly: true  },
 ];
 
 @Component({
--- client/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ client/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,12 +1,5 @@
 import {Routes} from '@angular/router';
-
-import {DashboardComponent} from '../../dashboard/dashboard.component';
 import {UserProfileComponent} from '../../user-profile/user-profile.component';
-import {TableListComponent} from '../../table-list/table-list.component';
-import {TypographyComponent} from '../../typography/typography.component';
-import {IconsComponent} from '../../icons/icons.component';
-import {NotificationsComponent} from '../../notifications/notifications.component';
-import {UpgradeComponent} from '../../upgrade/upgrade.component';
 import {ItemsComponent} from "../../items/items.component";
 import {CreateItemComponent} from "../../items/create-item/create-item.component";
 import {SaleComponent} from "../../sale/sale.component";
@@ -15,19 +8,15 @@ import {SaleDetailComponent} from "../../history/sale-detail/sale-detail.compone
 import {AddUserComponent} from "../../user-profile/add-user/add-user.component";
 
 export const AdminLayoutRoutes: Routes = [
-    {path: 'dashboard', component: DashboardComponent},
     {
         path: 'user-profile',
-    children: [
-        {path: '', component: UserProfileComponent},
-        {path: 'add', component: AddUserComponent}
-    ]},
-    {path: 'table-list', component: TableListComponent},
-    {path: 'typography', component: TypographyComponent},
-    {path: 'icons', component: IconsComponent},
-    {path: 'notifications', component: NotificationsComponent},
-    {path: 'upgrade', component: UpgradeComponent},
-    {path: 'sale', children:[
+        children: [
+            {path: '', component: UserProfileComponent},
+            {path: 'add', component: AddUserComponent}
+        ]
+    },
+    {
+        path: 'sale', children: [
             {path: '', component: SaleComponent}
         ]
     },
